b7c5d2f064bfe3edf90d53c8c2b81029e4f25e28
Alan <zeyu.weng@gmail.com>
2023-11-16 18:21:42 -0500

    initial file add

---
+++ server/server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = process.env.PORT || 3000
+
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+
+const io = new Server(server, {
+    cors: {
+      origin: "http://localhost:3001"
+    }
+});
+
+app.use(cors())
+
+io.on('connection', socket => {
+    console.log("a user connected: " + socket.id)
+
+    socket.on("send-wpm", (message, room) => {
+        if(socket.to(room) !== "") {
+            socket.to(room).emit("receive-message", message)
+        }
+    })
+
+    socket.on("join-room", room => {
+        socket.join(room)
+    })
+
+})
+
+app.get("/api", (req, res) => {
+    res.send({"data":"Hello world"})
+})
+
+server.listen(port, ()=> {
+    console.log("listening on port 3000")
+})
